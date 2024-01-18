b0476a640db50828acf2a7aa21ef4310473111d7
Keaton Hasse <git@keatonhasse.com>
2024-01-17 18:05:50 -0600

    more cleanup, some potential fixes for problematic episodes

--- database.py
+++ database.py
@@ -4,6 +4,9 @@ class Database:
     def __init__(self, name='tinydesk.db'):
         self.con = sqlite3.connect(name)
         self.create_tables()
+    
+    def close(self) -> None:
+        self.con.close()
 
     def create_tables(self) -> None:
         self.con.execute('''
@@ -17,17 +20,11 @@ class Database:
                 description TEXT NOT NULL
             )
         ''')
-        self.con.execute('''
-            CREATE TABLE IF NOT EXISTS page(
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                last_page INTEGER NOT NULL
-            )
-        ''')
         self.con.commit()
 
     def insert(self, episode: tuple[str, ...]) -> None:
-        self.con.execute(
-            '''INSERT INTO episodes(
+        self.con.execute('''
+                INSERT INTO episodes(
                 url,
                 title,
                 upload_date,
@@ -38,19 +35,15 @@ class Database:
         )
         self.con.commit()
 
-    def update_page(self, page: int) -> None:
-        self.con.execute(f'UPDATE page SET last_page = {page} WHERE id = 1')
-        self.con.commit()
-
     def select(self):
         pass
     
     def last_page(self) -> int:
         cur = self.con.cursor()
-        cur.execute('SELECT last_page FROM page WHERE id = 1')
+        cur.execute('SELECT COUNT(*) FROM episodes')
         return cur.fetchone()[0]
     
-    def check_url(self, url: str) -> bool:
+    def contains(self, url: str) -> bool:
         cur = self.con.cursor()
         cur.execute('SELECT COUNT(*) FROM episodes WHERE url = ?', (url,))
         return cur.fetchone()[0] != 0
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import json
+import re
 from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
-from fastapi import FastAPI
-from yt_dlp import YoutubeDL
+#from fastapi import FastAPI
+#from yt_dlp import YoutubeDL
 
 from database import Database
 
@@ -13,61 +14,65 @@ def request(url: str) -> BeautifulSoup:
     soup = BeautifulSoup(r.content, 'lxml')
     return soup
 
+def og_content(soup, type: str):
+    return soup.find('meta', {'property', f'og:{type}'})['content']
+
 def fetch_episode(url: str) -> None:
     print(f'fetching {url}')
     soup = request(url)
-    available = soup.find('p', class_='unvailable')
-    playlist = soup.find('ul', class_='playlist')
-    if available is None and playlist is None:
-        print('still fetching')
-        json_text = soup.find('script', {'type': 'application/ld+json'}).text
-        json_data = json.loads(json_text)
-        if 'subjectOf' in json_data:
-            episode_data = json_data['subjectOf'][0]
-            if 'embedUrl' in episode_data:
-                timestamp = int(datetime.fromisoformat(episode_data['uploadDate']).timestamp())
-                description = soup.find('meta', {'name': 'description'})['content']
-                description = description.replace('amp;', '')
-                episode = (
-                    url,
-                    episode_data['name'],
-                    timestamp,
-                    episode_data['thumbnailUrl'],
-                    episode_data['embedUrl'],
-                    description
-                )
-                db.insert(episode)
+    #available = soup.find('p', class_='unvailable')
+    #if available is None:
+    aria = soup.find('div', class_='speakable').find_all('b')
+    title = aria[0].text
+    timestamp = int(datetime.fromisoformat(soup.find('time')['datetime']).timestamp())
+    jw_player = soup.find('div', class_='graphicwrapper')
+    jw_player_json = json.loads(jw_player.find_all('div')[1]['data-jwplayer'])
+    thumbnail = jw_player_json['image']
+    content_url = jw_player_json['sources'][0]['file']
+    description = aria[1].text
+    episode = (
+        url,
+        title,
+        timestamp,
+        thumbnail,
+        content_url,
+        description
+    )
+    db.insert(episode)
 
 def fetch_episodes(url: str) -> None:
-    print('fetch episodes')
+    print('fetching episodes')
     soup = request(url)
     classes = ['item event-more-article', 'item event-more-article article-type-audio']
     episodes = soup.find_all('article', class_=classes)
+    #for episode in set(episodes).difference(bad_urls):
     for episode in episodes:
-        url = episode.find('a')['href']
-        if not db.check_url(url) and url not in skip_urls:
-            fetch_episode(url)
+        episode_url = episode.find('a')['href']
+        if not re.match('https:\/\/www.npr.org\/\d{4}\/\d{2}\/\d{2}\/\d{10}\/[\w-]+', episode_url):
+            continue
+        if not db.contains(episode_url):
+            fetch_episode(episode_url)
 
 def main() -> None:
+    base = 'https://www.npr.org/get/92071316'
+    fetch_episodes(f'{base}/render/partial/next?start=0')
     page = db.last_page()
-    print(page)
     while True:
-        r = requests.get(f'https://www.npr.org/get/92071316/remainingCount?start={page}')
-        remaining = int(r.text)
+        print(page)
+        params = f'start={page}'
+        remaining = int(requests.get(f'{base}/remainingCount?{params}').text)
         if remaining != 24:
             break
-        url = f'https://www.npr.org/get/92071316/render/partial/next?start={page}'
-        fetch_episodes(url)
+        fetch_episodes(f'{base}/render/partial/next?{params}')
         page += remaining
-        db.update_page(page)
-    print('all episode urls collected')
 
 if __name__ == '__main__':
-    skip_urls = (
-        'https://www.npr.org/2014/10/04/353534954/ryan-keberle-catharsis-tiny-desk-concert',
+    bad_urls = (
+        #'https://www.npr.org/2014/10/04/353534954/ryan-keberle-catharsis-tiny-desk-concert',
         'https://www.npr.org/2022/12/09/1140051996/eliane-elias-tiny-desk-concert',
-        'https://www.npr.org/series/761983313/tiny-desk-playlists',
-        'https://www.npr.org/music'
+        #'https://www.npr.org/2014/10/18/356901185/-sgeir-tiny-desk-concert',
+        #'https://www.npr.org/series/761983313/tiny-desk-playlists',
+        #'https://www.npr.org/music'
     )
     db = Database()
     main()
