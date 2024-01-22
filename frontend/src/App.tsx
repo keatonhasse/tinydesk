9d18bd5f59f69b173f5b46f082775bcabfcadcd9
keatonhasse <git@keatonhasse.com>
2024-01-22 00:03:52 -0600

    start a base for the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Episode {
+  id: string,
+  url: string,
+  title: string,
+  timestamp: number,
+  thumbnail: string
 }
 
-export default App
+function App() {
+  const [episodes, setEpisodes] = useState<Episode[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/episodes')
+      .then(async (res) => {
+        setEpisodes(await res.json());
+        setIsLoading(false);
+      })
+      .catch((error) => { console.log(error) })
+  }, [setEpisodes, isLoading]);
+
+  function getThumbnail(episode: Episode): string {
+    const date = new Date(episode.timestamp * 1000);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const tsDate = `${year}/${month}/${day}`;
+    return `https://media.npr.org/assets/img/${tsDate}/${episode.thumbnail}?s=200`;
+  }
+
+  function formatDate(episode: Episode): string {
+    const date = new Date(episode.timestamp * 1000);
+    return date.toLocaleString('en-US', {
+      // 12 hour > 24 hour
+      hour12: false,
+      month: 'long',
+      day: '2-digit',
+      year: 'numeric'
+    });
+  }
+
+  return (
+    <div>
+      {episodes.map((episode) => {
+        return (
+          <div key={episode.id}>
+            <img src={getThumbnail(episode)} />
+            <p>{episode.title}</p>
+            <p>{formatDate(episode)}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
